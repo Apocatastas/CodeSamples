89df0a2834ed9ec361b19a6ccb4e5f58abb0078b
ɐllɐ <32189772+Apocatastas@users.noreply.github.com>
2019-07-22 11:02:35 +0300

    initial version

---
+++ Broadcaster.cs
@@ -0,0 +1,429 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public static class Broadcaster
+{
+    private static GameObject prefabName;
+    private static GameControl gameController;
+    private static AudioSource soundHolder;
+    private static GameObject leavedAt;
+
+
+    private static string sub_month;
+    private static string sub_quartal;
+    private static string sub_year;
+    public static string sub_week;
+    private static string who;
+    private static string buttonValue;
+    private static bool isFreeLevel;
+    private static bool freePassed;
+    private static bool isFromGame;
+    private static Admob admob;
+    private static string main_mode_from;
+
+    public static string From
+    {
+        get
+        {
+            return main_mode_from;
+        }
+        set
+        {
+            main_mode_from = value;
+        }
+    }
+
+    public static Dictionary<string, int> PuzzlesPiecesToWin = new Dictionary<string, int>()
+    {
+        {"Dog",7 },
+        {"Rabbit",8},
+        {"Cat",8},
+        {"Hengehog",7},
+        {"Cow",8},
+        {"Donkey",8},
+        {"Eagle",8},
+        {"Sloth",7},
+        {"Whale",8},
+        {"Fish",8},
+        {"Panda",8},
+        {"Parrot",8},
+        {"Tiger",8},
+        {"Beer",8},
+        {"Cheetah",8},
+        {"Jiraffe",8},
+        {"Penguin",8},
+        {"Leo",8},
+        {"Lemur",8},
+        {"Pig",8},
+        {"Elephant",8},
+        {"Fox",8},
+        {"Owl",8},
+        {"Squirrel",8},
+        {"Hamster",8},
+        {"Bat",8},
+        {"Racoon",8},
+        {"Tortoise",8},
+        {"Monkey",8},
+        {"Chameleon",8},
+        {"Luigi",13},
+        {"Guido",16},
+        {"Monty",12},
+        {"Sally",13},
+        {"Sheriff",13},
+        {"Dino1",11},
+        {"Dino2",18},
+        {"Dino3",14},
+        {"Dino4",12},
+        {"Dino5",13},
+    };
+    public static Dictionary<string, string> PuzzlesWho = new Dictionary<string, string>()
+    {
+        {"Dog","Animal"},
+        {"Rabbit","Animal"},
+        {"Cat","Animal"},
+        {"Hengehog","Animal"},
+        {"Cow","Animal"},
+        {"Donkey","Animal"},
+        {"Eagle","Animal"},
+        {"Sloth","Animal"},
+        {"Whale","Animal"},
+        {"Fish","Animal"},
+        {"Panda","Animal"},
+        {"Parrot","Animal"},
+        {"Tiger","Animal"},
+        {"Beer","Animal"},
+        {"Cheetah","Animal"},
+        {"Jiraffe","Animal"},
+        {"Penguin","Animal"},
+        {"Leo","Animal"},
+        {"Lemur","Animal"},
+        {"Pig","Animal"},
+        {"Elephant","Animal"},
+        {"Fox","Animal"},
+        {"Owl","Animal"},
+        {"Squirrel","Animal"},
+        {"Hamster","Animal"},
+        {"Bat","Animal"},
+        {"Racoon","Animal"},
+        {"Tortoise","Animal"},
+        {"Monkey","Animal"},
+        {"Chameleon","Animal"},
+        {"Luigi","Car"},
+        {"Guido","Car"},
+        {"Monty","Car"},
+        {"Sally","Car"},
+        {"Sheriff","Car"},
+        {"Dino1","Dino"},
+        {"Dino2","Dino"},
+        {"Dino3","Dino"},
+        {"Dino4","Dino"},
+        {"Dino5","Dino"},
+    };
+    public static Dictionary<string, int> PuzzlesIsFree = new Dictionary<string, int>()
+    {
+        {"Dog",1},
+        {"Rabbit",1},
+        {"Cat",1},
+        {"Hengehog",1},
+        {"Cow",1},
+        {"Donkey",1},
+        {"Eagle",1},
+        {"Sloth",1},
+        {"Whale",1},
+        {"Fish",1},
+        {"Panda",1},
+        {"Parrot",1},
+        {"Tiger",0},
+        {"Beer",0},
+        {"Cheetah",0},
+        {"Jiraffe",0},
+        {"Penguin",0},
+        {"Leo",0},
+        {"Lemur",0},
+        {"Pig",0},
+        {"Elephant",0},
+        {"Fox",0},
+        {"Owl",0},
+        {"Squirrel",0},
+        {"Hamster",0},
+        {"Bat",0},
+        {"Racoon",0},
+        {"Tortoise",0},
+        {"Monkey",0},
+        {"Chameleon",0},
+        {"Luigi",1},
+        {"Guido",1},
+        {"Monty",1},
+        {"Sally",1},
+        {"Sheriff",1},
+        {"Dino1",1},
+        {"Dino2",1},
+        {"Dino3",1},
+        {"Dino4",1},
+        {"Dino5",1},
+    };
+    public static string[] pref_num = new string[]
+        {
+            "Dog",
+            "Rabbit",
+            "Cat",
+
+            "Hengehog",
+            "Cow",
+            "Donkey",
+
+            "Eagle",
+            "Sloth",
+            "Whale",
+
+
+            "Fish",
+            "Panda",
+            "Parrot",
+
+            "Tiger",
+            "Beer",
+            "Cheetah",
+
+             "Jiraffe",
+             "Penguin",
+             "Leo",
+
+             "Lemur",
+             "Pig",
+             "Elephant",
+
+            "Fox",
+            "Owl",
+            "Squirrel",
+
+            "Hamster",
+            "Bat",
+            "Racoon",
+
+            "Tortoise",
+            "Monkey",
+            "Chameleon"
+             };
+    public static string[] pref_num_cars = new string[]
+        {
+            "Luigi",
+            "Guido",
+            "Sheriff",
+            "Sally",
+            "Monty"
+             };
+    public static string[] pref_num_dinos = new string[]
+        {
+            "Dino1",
+            "Dino2",
+            "Dino3",
+            "Dino4",
+            "Dino5"
+             };
+
+    public static Admob AdHolder
+    {
+        get
+        {
+            return admob;
+        }
+        set
+        {
+            admob = value;
+        }
+    }
+    public static GameObject Prefab
+    {
+        get
+        {
+            return prefabName;
+        }
+        set
+        {
+            prefabName = value;
+        }
+    }
+
+    
+
+
+    public static GameObject lastPlayed
+    {
+        get
+        {
+            return leavedAt;
+        }
+        set
+        {
+            leavedAt = value;
+        }
+    }
+    public static AudioSource sounds
+    {
+        get
+        {
+            return soundHolder;
+        }
+        set
+        {
+            soundHolder = value;
+        }
+    }
+    public static GameControl Game
+    {
+        get
+        {
+            return gameController;
+        }
+        set
+        {
+            gameController = value;
+        }
+    }
+
+
+
+    public static string Month
+    {
+        get
+        {
+            return sub_month;
+        }
+        set
+        {
+            sub_month = value;
+        }
+    }
+    public static string ParentalButton
+    {
+        get
+        {
+            return buttonValue;
+        }
+        set
+        {
+            buttonValue = value;
+        }
+    }
+    public static string Week
+    {
+        get
+        {
+            return sub_week;
+        }
+        set
+        {
+            sub_week = value;
+        }
+    }
+    public static string Year
+    {
+        get
+        {
+            return sub_year;
+        }
+        set
+        {
+            sub_year = value;
+        }
+    }
+    public static string Quartal
+    {
+        get
+        {
+            return sub_quartal;
+        }
+        set
+        {
+            sub_quartal = value;
+        }
+    }
+    public static string Who
+    {
+        get
+        {
+            return who;
+        }
+        set
+        {
+            who = value;
+        }
+    }
+    public static bool isFreeLvl
+    {
+        get
+        {
+            return ((who == "Dog")
+                || (who == "Rabbit")
+                || (who == "Cat")
+                || (who == "Hengehog")
+                || (who == "Cow")
+                || (who == "Donkey")
+                || (who == "Eagle")
+                || (who == "Sloth")
+                || (who == "Whale")
+                || (who == "Fish")
+                || (who == "Panda")
+                || (who == "Luigi")
+                || (who == "Guido")
+                || (who == "Sally")
+                || (who == "Monty")
+                || (who == "Sheriff")
+                || (who == "Dino1")
+                || (who == "Dino2")
+                || (who == "Dino3")
+                || (who == "Dino4")
+                || (who == "Dino5")
+                || (who == "Parrot"));
+        }
+        set
+        {
+            isFreeLevel = value;
+        }
+    }
+    public static bool isEPFromGame
+    {
+        get
+        {
+            return isFromGame;
+        }
+        set
+        {
+            isFromGame = value;
+        }
+    }
+    public static bool freeLevelsPassed
+    {
+        get
+        {
+            return ((PlayerPrefs.GetInt("Dog") == 1) &&
+            (PlayerPrefs.GetInt("Rabbit") == 1) &&
+            (PlayerPrefs.GetInt("Cat") == 1) &&
+            (PlayerPrefs.GetInt("Hengehog") == 1) &&
+            (PlayerPrefs.GetInt("Cow") == 1) &&
+            (PlayerPrefs.GetInt("Donkey") == 1) &&
+            (PlayerPrefs.GetInt("Eagle") == 1) &&
+            (PlayerPrefs.GetInt("Sloth") == 1) &&
+            (PlayerPrefs.GetInt("Whale") == 1) &&
+            (PlayerPrefs.GetInt("Fish") == 1) &&
+            (PlayerPrefs.GetInt("Panda") == 1) &&
+            (PlayerPrefs.GetInt("Parrot") == 1) &&
+            (PlayerPrefs.GetInt("Dino1") == 1) &&
+            (PlayerPrefs.GetInt("Dino2") == 1) &&
+            (PlayerPrefs.GetInt("Dino3") == 1) &&
+            (PlayerPrefs.GetInt("Dino4") == 1) &&
+            (PlayerPrefs.GetInt("Dino5") == 1) &&
+            (PlayerPrefs.GetInt("Sally") == 1) &&
+            (PlayerPrefs.GetInt("Monty") == 1) &&
+            (PlayerPrefs.GetInt("Sheriff") == 1) &&
+            (PlayerPrefs.GetInt("Luigi") == 1) &&
+            (PlayerPrefs.GetInt("Guido") == 1));
+        }
+        set
+        {
+            freePassed = value;
+        }
+    }
+}
---
+++ Purchaser.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using com.adjust.sdk;
+using UnityEngine.UI.Extensions;
+using UnityEngine.UI;
+
+
+
+
+public class Purchaser : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController;          // The Unity Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider; // The store-specific Purchasing subsystems.
+
+    public static string WEEK_SUB = "week_subscription";
+    public GameObject subInterface;
+    public GameObject noSubInterface;
+    public GameObject parentalInterface;
+    public GameObject mainScreen;
+    public GameObject uiCanvas;
+    public Text debugInfo;
+    public Text price;
+    public Text currency;
+    public string priceString;
+    public bool itsTime = false;
+    public Assets.SimpleLocalization.MenuController m_menuController;
+    private static Dictionary<string, string> introductory_info_dict;
+    public bool isSubscriptionExpired;
+    public bool isSubscribed;
+    public bool isInited;
+
+    public System.Collections.IEnumerator SubChecker() //IF WE GOT CONTROLLA WE SET PREFS
+    {
+        debugInfo.text += " SCH ";
+        PlayerPrefs.SetInt("isSubscriber", 0);
+        if (m_StoreController != null && m_StoreExtensionProvider != null)
+        {
+            if (m_StoreController.products.WithID("week_subscription").receipt != null)
+            {
+                if (m_StoreController.products.WithID("week_subscription").definition.type == ProductType.Subscription)
+                {
+
+                    if (checkIfProductIsAvailableForSubscriptionManager(m_StoreController.products.WithID("week_subscription").receipt))
+                    {
+                        string intro_json =
+                         (introductory_info_dict == null
+                          || !introductory_info_dict.ContainsKey(m_StoreController.products.WithID("week_subscription").definition.storeSpecificId))
+                          ? null
+                          : introductory_info_dict[m_StoreController.products.WithID("week_subscription").definition.storeSpecificId];
+                        SubscriptionManager p = new SubscriptionManager(m_StoreController.products.WithID("week_subscription"), intro_json);
+                        SubscriptionInfo info = p.getSubscriptionInfo();
+                        Broadcaster.Week = m_StoreController.products.WithID(WEEK_SUB).metadata.localizedPriceString;
+                        if (info.isSubscribed() == Result.True)
+                        {
+                            PlayerPrefs.SetInt("isSubscriber", 1);
+                            debugInfo.text += " SCH1 ";
+                        }
+                        else
+                        {
+                            debugInfo.text += " SCH0 ";
+                        }
+                    }
+                }
+            }
+        }
+        yield return 0;
+    }
+
+    public System.Collections.IEnumerator SubSwitcher() //PREFS SWITCHER
+    {
+        debugInfo.text += " SSW ";
+        uiCanvas.SetActive(true);
+        switch (PlayerPrefs.GetInt("isSubscriber"))
+        {
+            case 1:
+                subInterface.SetActive(true);
+                noSubInterface.SetActive(false);
+                debugInfo.text += " SSW1 ";
+                break;
+            case 0:
+                subInterface.SetActive(false);
+                noSubInterface.SetActive(true);
+                debugInfo.text += " SSW0 ";
+                break;
+        }
+        mainScreen.SetActive(false);
+        yield return 0;
+    }
+
+    public void SendSuccessEvent()
+    {
+        debugInfo.text += " goAmp ";
+        Amplitude.Instance.logEvent("SuccessBuy");
+        Amplitude.Instance.setUserProperty("EP", "Subscr");
+        if (PlayerPrefs.GetInt("FirstEP") == 1) { SendAdjustEvent("po56v3"); PlayerPrefs.SetInt("FirstEP", 0); };
+        if (PlayerPrefs.GetInt("AnimationPushed") == 1) { SendAdjustEvent("lbdcaq"); PlayerPrefs.SetInt("AnimationPushed", 0); };
+        if (PlayerPrefs.GetInt("CartPushed") == 1) { SendAdjustEvent("lkg0ns"); PlayerPrefs.SetInt("CartPushed", 0); };
+
+    }
+
+    public System.Collections.IEnumerator GetScreen()
+    {
+        debugInfo.text += " GS ";
+        StartCoroutine("SubChecker");
+
+        yield return new WaitForSeconds(2f);
+        StartCoroutine("SubSwitcher");
+
+        yield return 0;
+    }
+       public void BuySubscription(string productId)
+    {
+        BuyProductID(productId);
+        InitSubscription(m_StoreController.products.WithID(productId));
+    }
+    private void InitSubscription(Product item) //THIS IS FOR DYNAMIC SWITCHING AFTER PURCHASE
+    {
+        if (item.receipt != null)
+        {
+            if (item.definition.type == ProductType.Subscription)
+            {
+                if (checkIfProductIsAvailableForSubscriptionManager(item.receipt))
+                {
+                    string intro_json =
+                     (introductory_info_dict == null
+                      || !introductory_info_dict.ContainsKey(item.definition.storeSpecificId))
+                      ? null
+                      : introductory_info_dict[item.definition.storeSpecificId];
+                    SubscriptionManager p = new SubscriptionManager(item, intro_json);
+                    SubscriptionInfo info = p.getSubscriptionInfo();
+
+                    if (info.isSubscribed() == Result.True)
+                    {
+                        SendSuccessEvent();
+                        PlayerPrefs.SetInt("isSubscriber", 1);
+                        StartCoroutine("GetScreen");
+                    }
+                    else
+                    {
+                        if (info.isExpired() == Result.True)
+                        {
+                            PlayerPrefs.SetInt("isSubscriber", 0);
+                            StartCoroutine("GetScreen");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #region STANDART FUNC
+    public bool WeHaveStore()
+    {
+        if (m_StoreController != null && m_StoreExtensionProvider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void Start()
+    {
+        if (m_StoreController == null)
+        {
+            InitializePurchasing();
+   //         StartCoroutine("Pricer");
+        }
+    }
+
+ //   public System.Collections.IEnumerator Pricer()
+ //   {
+ //       yield return new WaitForSeconds(3f);
+        //price.text = m_StoreController.products.WithID(WEEK_SUB).metadata.localizedPriceString;
+//        price.text = m_StoreController.products.WithID(WEEK_SUB).metadata.localizedPrice.ToString();
+//        currency.text = m_StoreController.products.WithID(WEEK_SUB).metadata.isoCurrencyCode;
+//        yield return 0;
+//    }
+
+    public void InitializePurchasing() //builder add products
+    {
+        if (m_StoreController != null && m_StoreExtensionProvider != null)
+        {
+            return;
+        }
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct(WEEK_SUB, ProductType.Subscription, new IDs(){
+
+            { WEEK_SUB, GooglePlay.Name },
+        });
+
+        UnityPurchasing.Initialize(this, builder);
+        //  Debug.Log("BUILDER " + Broadcaster.Week);
+        
+
+
+    }
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+        isInited = true;
+    }
+    private void OnDeferred(Product item)
+    {
+    }
+    public void SendAdjustEvent(string what)
+    {
+        AdjustEvent adjustEvent = new AdjustEvent(what);
+        Adjust.trackEvent(adjustEvent);
+    }
+    public void BuyProductID(string productId)
+    {
+        // If Purchasing has been initialized ...
+        if (m_StoreController != null && m_StoreExtensionProvider != null)
+        {
+            Product product = m_StoreController.products.WithID(productId);
+            if (product != null)
+            {// Debug.Log("buy prod is not null" + product.definition.id); 
+            }
+
+            if (product.availableToPurchase)
+            { //Debug.Log("buy prod is ready to purchase"); 
+            }
+
+            // If the look up found a product for this device's store and that product is ready to be sold ... 
+            if (product != null && product.availableToPurchase)
+            {
+                //Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                // ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed 
+                // asynchronously.
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                // Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+
+            }
+        }
+        else
+        {
+            // Debug.Log("BuyProductID FAIL. Not initialized.");
+
+        }
+    }
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        // Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, WEEK_SUB, StringComparison.Ordinal))
+        {
+            InitSubscription(args.purchasedProduct);
+        }
+        return PurchaseProcessingResult.Complete;
+    }
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        // A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing 
+        // this reason with the user to guide their troubleshooting actions.
+        //   Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+
+    }
+    private static bool checkIfProductIsAvailableForSubscriptionManager(string receipt)
+    {
+        var receipt_wrapper = (Dictionary<string, object>)MiniJson.JsonDecode(receipt);
+        if (!receipt_wrapper.ContainsKey("Store") || !receipt_wrapper.ContainsKey("Payload"))
+        {
+            //     Debug.Log("The product receipt does not contain enough information");
+
+            return false;
+        }
+
+        var store = (string)receipt_wrapper["Store"];
+        var payload = (string)receipt_wrapper["Payload"];
+
+        if (payload != null)
+        {
+            switch (store)
+            {
+                case GooglePlay.Name:
+                    {
+                        var payload_wrapper = (Dictionary<string, object>)MiniJson.JsonDecode(payload);
+                        if (!payload_wrapper.ContainsKey("json"))
+                        {
+                            //   Debug.Log(
+                            //    "The product receipt does not contain enough information, the 'json' field is missing");
+
+                            return false;
+                        }
+
+                        var original_json_payload_wrapper =
+                         (Dictionary<string, object>)MiniJson.JsonDecode((string)payload_wrapper["json"]);
+                        if (original_json_payload_wrapper == null
+                            || !original_json_payload_wrapper.ContainsKey("developerPayload"))
+                        {
+                            //  Debug.Log(
+                            //  "The product receipt does not contain enough information, the 'developerPayload' field is missing");
+
+                            return false;
+                        }
+
+                        var developerPayloadJSON = (string)original_json_payload_wrapper["developerPayload"];
+                        var developerPayload_wrapper =
+                         (Dictionary<string, object>)MiniJson.JsonDecode(developerPayloadJSON);
+                        if (developerPayload_wrapper == null || !developerPayload_wrapper.ContainsKey("is_free_trial")
+                                                             || !developerPayload_wrapper.ContainsKey(
+                                                              "has_introductory_price_trial"))
+                        {
+                            //  Debug.Log(
+                            //  "The product receipt does not contain enough information, the product is not purchased using 1.19 or later");
+
+                            return false;
+                        }
+
+
+                        return true;
+                    }
+                case AppleAppStore.Name:
+                case AmazonApps.Name:
+                case MacAppStore.Name:
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+
+        return false;
+    }
+    public void CheckAllProducts(Product[] products, Dictionary<string, string> dict)
+    {
+        introductory_info_dict = dict;
+        foreach (var item in products)
+        {
+            if (item.availableToPurchase)
+            {
+                InitSubscription(item);
+            }
+        }
+    }
+    #endregion STANDART FUNC
+}
+
